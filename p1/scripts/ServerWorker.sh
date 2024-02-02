f7c26198c3b7fa700d54ee908cb5b0dc160f4851
Nathan Guiard <nguiard@student.42.fr>
2024-02-02 01:58:03 -0600

    Displays IP on login, prettier script logs and gitkeep on p1/synced folder

--- p1/scripts/ServerWorker.sh
+++ p1/scripts/ServerWorker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "\033[1;3;33m--- Server Worker script starting ---\033[0m"
+echo -e "\033[1;3;33m--- Server Worker script starting ---\033[0m"
 
 apt-get update -y && apt-get install -y curl
 
-echo "\033[1;32m--- Installing K3s ---\033[0m"
+echo -e "\033[1;32m--- Installing K3s ---\033[0m"
 
 # --flannel-iface is needed for k3s to communicate on our 'private_network' interface
 curl -sfL https://get.k3s.io | K3S_URL="https://$SERVER_IP:6443" \
@@ -12,4 +12,10 @@ curl -sfL https://get.k3s.io | K3S_URL="https://$SERVER_IP:6443" \
 	INSTALL_K3S_EXEC="--flannel-iface=eth1" \
 	sh -
 
-echo "\033[1;3;33m--- Server Worker script finished ---\033[0m"
+echo "
+echo
+echo -e '\033[1mifconfig eth1 (ip addr | grep 'eth1:' -A 6):\033[0m'
+ip addr | grep 'eth1:' -A 6
+" >> /home/vagrant/.bashrc
+
+echo -e "\033[1;3;33m--- Server Worker script finished ---\033[0m"
