d6fea8ff48908faf6b38cf1cb98a701b67de6018
Nathan Guiard <nguiard@student.42.fr>
2024-02-05 12:57:24 -0600

    gitlab is now running. Just need to connect the two

--- bonus/everything.sh
+++ bonus/everything.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+if [ $(whoami) != root ]; then
+	echo 'You must be root'
+	exit 1
+fi
+
 run() {
 	echo -e "\033[1;34m--- INSTALLATION ---\033[0m"
-	if [ -x /usr/local/bin/k3d ] && \
-		[ -x /usr/local/bin/kubectl ] && \
-		[ -x /usr/bin/docker ]
+	if which k3d && which kubectl && \
+		which docker && which helm
 	then
 		echo "Everything seem to be already installed, skipping installation."
 	else
@@ -12,52 +16,58 @@ run() {
 	fi
 
 	echo -e "\033[1;34m--- SETUP ---\033[0m"
-	if [ -x /usr/local/bin/k3d ] && \
-		[ -x /usr/local/bin/kubectl ] && \
-		[ -x /usr/bin/docker ]
-	then
-		echo "Everything seem to be already installed, skipping installation."
-	else
-		bash scripts/installation.sh
-	fi
+	bash scripts/setup.sh
+	echo 'OK'
 }
 
 stop() {
 	k3d cluster delete bonus
 	rm argoCD.password
 	rm gitlab.password
+	echo 'OK'
 }
 
 clean() {
+	stop
+	rm $(which helm)
+	rm -rf ~/.config/helm
+	rm -rf ~/.cache/helm
+	rm -rf ~/.local/share/helm
+	k3d cluster delete -a
+	rm $(which k3d)
+	rm $(which kubectl)
+	rm -rf ~/.kube
 	apt-get remove -y docker-ce docker-ce-cli containerd.io \
 		docker-buildx-plugin docker-compose-plugin
-	
+	apt autoremove -y
 }
 
 if [ -z "$1" ]
 then
+	PS3="> "
 	select option in run stop clean
 	do
-		case $lng in
+		echo $option
+		case $option in
 			"run")
-				run;;
+				run; exit;;
 			"stop")
-				stop;;
+				stop; exit;;
 			"clean")
-				echo test;;
+				clean; exit;;
 			*)
-			echo "Choose from: run/stop/clean";;
+				echo -e 'Choose from:\nrun (1)\nstop (2)\nclean (3)';;
 		esac
 	done
 else
 	case $1 in
 		"run")
-			run;;
+			run; exit;;
 		"stop")
-			stop;;
+			stop; exit;;
 		"clean")
-			echo test;;
+			clean; exit;;
 		*)
-		echo "Choose from: run/stop/clean"; exit 1;
+			echo "Choose from: run/stop/clean"; exit 1;;
 	esac
 fi
--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -25,13 +25,6 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 
 sleep 10
 
-kubectl wait --timeout=200s --for=condition=Ready -n argocd --all pod
-
-# https://stackoverflow.com/questions/68297354/what-is-the-default-password-of-argocd
-echo -e "\033[1mArgoCD password:"
-kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode > argoCD.password
-cat argoCD.password
-echo -e "\nSaved to argoCD.password\033[0m"
 
 if [ -f /vagrant/confs/argocd-deploy.yml ]; then
 	kubectl apply -f /vagrant/confs/argocd-deploy.yml -n argocd
@@ -48,17 +41,35 @@ helm upgrade --install gitlab gitlab/gitlab \
   --set global.hosts.domain=gitlab.local \
   --set global.hosts.externalIP=0.0.0.0 \
   --set global.hosts.https=false \
+  --values https://gitlab.com/gitlab-org/charts/gitlab/-/raw/master/examples/values-minikube-minimum.yaml \
   --set certmanager-issuer.email=nguiard@student.42.fr \
   --set postgresql.image.tag=13.6.0 \
   -n gitlab
 
-echo 'WAITING FOR GITLAB TO BE READY (timeout 10 min)'
-kubectl wait --for=condition=ready --timeout=600s pod -l app=webservice -n gitlab
+sleep 30
+
+kubectl wait --timeout=200s --for=condition=Ready -n argocd --all pod
+# https://stackoverflow.com/questions/68297354/what-is-the-default-password-of-argocd
+echo -e "\033[1mArgoCD password:"
+kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode > argoCD.password
+echo >> argoCD.password
+cat argoCD.password
+echo -e "Saved to argoCD.password\033[0m"
 
 echo -e "\033[1mGitlab password:"
 kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode > gitlab.password
+echo >> gitlab.password
 cat gitlab.password
-echo -e "\nSaved to gitlab.password\033[0m"
+echo -e "Saved to gitlab.password\033[0m"
 
-kubectl port-forward service/argocd-server --address 0.0.0.0 -n argocd 8080:443 2>&1 >/dev/null
-kubectl port-forward service/gitlab-webservice-default --address 0.0.0.0 -n gitlab 8181:8181 2>&1 >/dev/null
+# Port mapping
+echo -e '\033[32mWaiting for argoCD to be ready (3min)\033[0m'
+kubectl wait --for=condition=ready --timeout=180s pod -l app.kubernetes.io/name=argocd-server -n argocd
+echo 'Port mapping argoCD 8080:433'
+kubectl port-forward service/argocd-server --address 0.0.0.0 -n argocd 8080:443 2>&1 >/dev/null & disown
+echo -e '\n\033[32mWaiting for Gitlab to be ready (10min)\033[0m'
+kubectl wait --timeout=600s --for=condition=Ready -n gitlab --all pod
+echo 'Port mapping Gitlab 8181:8181'
+kubectl port-forward service/gitlab-webservice-default --address 0.0.0.0 -n gitlab 8181:8181 2>&1 >/dev/null & disown
+
+sleep 1
